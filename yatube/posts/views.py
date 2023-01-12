5a8b10123cc29aa90fc8b17827d3ebfebeb95e41
Сергей Емельянов <3201888@mail.ru>
2023-01-12 18:24:43 +0300

    2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,24 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
-
 def index(request):
-    return HttpResponse('главная старница')
+    template = 'posts/index.html'
+    title = 'главная страница'
+    context = {
+        'title': title
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, any_slug):
-    return HttpResponse('Группы')
+    template = 'posts/group_list.html'
+    title = 'информация о группах'
+    context = {
+        'title': title
+    }
+    return render(request, template, context)
+
+
+
